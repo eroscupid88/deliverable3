9b89deeccedfcc9647cbf90aa999b994c687fad1
Dillon Vu <vutuanduong2003@yahoo.com>
2023-03-08 14:44:33 -0600

    change

--- exam/rpi4.py
+++ exam/rpi4.py
@@ -49,10 +49,10 @@ class Rpi4(object):
             # data = ' '.join(str(i) for i in self.parking_data) +"\n"+ sensor.toString()
             # if (self.response):
             self.mqttClient.subscribe(self.mqttClient.client,self.topic1)
-            if (self.mqttClient.message_to_rpi == '1'):
-                self.received =True
-                self.takePicture()
-                self.mqttClient.run_publish(self.topic,self.response())
+            # if (self.mqttClient.message_to_rpi == '1'):
+            #     self.received =True
+            #     self.takePicture()
+            #     self.mqttClient.run_publish(self.topic,self.response())
             
             # self.run_display()
 
